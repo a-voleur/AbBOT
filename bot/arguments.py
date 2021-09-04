6b7d017616dcfce5b9c6e52cc717dd0275125dca
a-voleur <90015194+a-voleur@users.noreply.github.com>
2021-09-03 20:33:04 -0500

    oopsi

--- bot/arguments.py
+++ bot/arguments.py
@@ -3,6 +3,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', help='Increases the verbosity of the output.', action='store_true')
 parser.add_argument('-c', '--count', help='Set a maximum number of times to successfully submit to the form.', type=int, default=None)
-parser.add_argument('-t', '--text', help='Use "wacky" or "real" to generate wacky or realistic-looking form responses. Default is realistic-looking', type=int, default='real')
+parser.add_argument('-t', '--text', help='Use "wacky" or "real" to generate wacky or realistic-looking form responses. Default is realistic-looking', type=str, default='real')
 
 args = parser.parse_args()
